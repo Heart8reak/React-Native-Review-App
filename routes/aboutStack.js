87b76d5ef829e4cd9376379d53f7484b7971a7d8
Carlos Baez <c.baez1223@yahoo.com>
2020-12-15 21:12:17 -0500

    first commit completed react-native app

---
+++ routes/aboutStack.js
@@ -0,0 +1,25 @@
+import {createStackNavigator } from 'react-navigation-stack'
+import Header from '../shared/header'
+import React from 'react'
+
+import About from '../screens/about'
+
+const screens = {
+    About: {
+        screen: About,
+        navigationOptions: ({ navigation }) => {
+            return {
+                headerTitle: () => <Header navigation={navigation} title='About GameZone' />,
+            }
+        }
+    },
+}
+
+const AboutStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: '#444',
+        headerStyle: { backgroundColor: '#eee', height: 80 }
+    }
+})
+
+export default AboutStack
